e45375d9d74f3182b17a324d32f8019c98e2abf4
carson-v <carson.vandegriffe@gmail.com>
2021-02-03 09:45:07 -0800

    same fix

--- camb_model/testing_data/preprocess.py
+++ camb_model/testing_data/preprocess.py
@@ -207,7 +207,7 @@ def remove_emoji(string):
 
 
 def remove_special_characters(text, remove_digits=False):
-    pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+    pattern = r'[^a-zA-z0-9\s]|\^' if not remove_digits else r'[^a-zA-z\s]'
     text = re.sub(pattern, ' ', text)
     return text
 
